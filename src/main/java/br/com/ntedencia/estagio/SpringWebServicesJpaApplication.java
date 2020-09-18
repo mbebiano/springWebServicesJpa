d1b1cb03ccf3c6a4655c48d931c7cc9a533040a0
mbebiano <mattheus.bebiano@engenharia.ufjf.br>
2020-09-18 05:49:33 -0300

    Create Project

---
+++ src/main/java/br/com/ntedencia/estagio/SpringWebServicesJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ntedencia.estagio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebServicesJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebServicesJpaApplication.class, args);
+	}
+
+}
